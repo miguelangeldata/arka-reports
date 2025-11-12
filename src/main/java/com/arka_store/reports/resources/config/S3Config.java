9e93c413e655db6f7b2edac905703aa5b27ea9fe
Miguel Angel Cardona <miguel.acardona98@gmail.com>
2025-11-12 14:50:48 -0500

    s3

--- src/main/java/com/arka_store/reports/resources/config/S3Config.java
+++ src/main/java/com/arka_store/reports/resources/config/S3Config.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
 
@@ -28,12 +29,17 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))  // 🔥 Usa localstack interno
                 .region(Region.of(region))
-                .endpointOverride(URI.create(endpoint))
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
                                 AwsBasicCredentials.create(accessKey, secretKey)
                         )
+                )
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
                 )
                 .build();
     }
